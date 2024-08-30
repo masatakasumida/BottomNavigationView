0048f75bb2590566b2e0286e5aa6b773881d0542
masatakasumida <masasumi0327@gmail.com>
2024-08-30 12:38:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.bottomnavigationview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.bottomnavigationview.databinding.ActivityMainBinding // ビューバインディングクラスをインポート
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.location -> replaceFragment(LocationFragment())
+                R.id.settings -> replaceFragment(SettingsFragment())
+
+                else -> {
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+}
